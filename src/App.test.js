49705aae039af234673caa1422489ad99a42cd0d
David Oliveira <davidstna21@gmail.com>
2024-05-21 21:39:44 -0300

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+describe('Jest', () => {
+    it('should work', () => {
+        expect(1).toBe(1);
+    })
+    it('is working', () => {
+        expect(2).toBe(2);
+    })
+    it('is ready', () => {
+        expect(3).toBe(3);
+    })
+})
